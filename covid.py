f59d510304373215e9df492751e640e8803b7d56
fajisnm <87913488+fajisnm@users.noreply.github.com>
2021-08-07 14:41:25 +0530

    Update covid.py

--- covid.py
+++ covid.py
@@ -1,11 +1,4 @@
-async def corona(event):
-    "To get latest information about covid-19."
-    input_str = event.pattern_match.group(1)
-    country = (input_str).title() if input_str else "World"
-    catevent = await edit_or_reply(event, "`Collecting data...`")
-    covid = Covid(source="worldometers")
-    try:
-        country_data = covid.get_status_by_country_name(country)
+ country_data = covid.get_status_by_country_name(country)
     except ValueError:
         country_data = ""
     if country_data:
